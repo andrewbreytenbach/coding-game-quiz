e215bb26c42d05d512d4f9f6e6d2665fcb9c5c6d
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-15 09:35:44 -0500

    fixed the timer interval so that it correctly displays the timer on the HTML

--- script.js
+++ script.js
@@ -48,14 +48,14 @@ var questionFive = {
   }
 
 // Created a function that will display the remaining time in the game to the HTML.
-document.querySelector(".start-button").addEventListener("click", function() {
+window.onload = function() { 
     var timeLeft = 75;
 
     var timeInterval = setInterval(function() {
         timeLeft--;
-        document.querySelector(".timer-count").textContent = timeLeft + " seconds left"
+        document.querySelector("#timer").textContent = timeLeft + " seconds left"
         if (timeLeft === 0) {
             clearInterval(timeInterval)
         }
     }, 1000);
-})
+};
