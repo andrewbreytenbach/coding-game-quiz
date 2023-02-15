85682ed3f0d87fbb84856fe417671cf2f23d5a41
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-15 16:30:25 -0500

    added new structure to the html in order to get the question to display. Still not all the way there.

--- script.js
+++ script.js
@@ -20,24 +20,28 @@ const quizArray = [
       options: ["strings", "numbers", "alerts", "booleans"],
       correct: "alerts",
     },
+
     {
       id: "1",
       question: "The condition in an if/else statement is enclosed with ________",
       options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
       correct: "curly brackets",
     },
+
     {
       id: "2",
       question: "Arrays in JavaScript can be used to store _________",
       options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
       correct: "all of the above",
     },
+
     {
       id: "3",
       question: "String values must be enclosed within _________ when being assigned to variables.",
       options: ["commas", "curly brackets", "quotes", "parenthesis"],
       correct: "quotes",
     },
+    
     {
       id: "4",
       question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
@@ -58,4 +62,12 @@ const quizArray = [
   };
 
 showQuestion(quizArray[0]);
+
+
+
+// This part of the js code deals with the local storage and storing the high score of the game after each time it is played. 
+localStorage.setItem('highScore', 'score');
+
+
+var highScore = localStorage.getItem('highScore');
 
