7aa5cd41c7c833e2b56ed1c4dc860b568adc9949
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-17 12:44:56 -0500

    made changes in the js file so the timer doesn't reset for each question

--- script.js
+++ script.js
@@ -1,76 +1,80 @@
 // Used query selector to set all the variables I will need for the function.
 let timeLeft = document.querySelector(".time-left");
-let quizContainer = document.getElementById("#questions-container");
-let submitBtn = document.getElementById("#submit-button");
+let quizContainer = document.getElementById("container");
+let submitBtn = document.getElementById("submit-button");
 let countOfQuestion = document.querySelector(".number-of-question");
-let displayContainer = document.getElementById("#display-container");
+let displayContainer = document.getElementById("display-container");
 let scoreContainer = document.querySelector(".score-container");
 let restart = document.getElementById("restart");
-let userScore = document.getElementById("#user-score");
+let userScore = document.getElementById("user-score");
 let startScreen = document.querySelector(".start-screen");
-let startButton = document.getElementById("#start-button");
+let startButton = document.getElementById("start-button");
 let questionCount;
 let scoreCount = 0;
 let count = 75;
+let startTime, elapsedTime = 0;
 let countdown;
 
 // Created an array with the quiz questions inside of it.
 const quizArray = [
     {
-      id: "0",
-      question: "Commonly used data types DO NOT include:",
-      options: ["strings", "numbers", "alerts", "booleans"],
-      correct: "alerts",
+        id: "0",
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "numbers", "alerts", "booleans"],
+        correct: "alerts",
     },
     {
-      id: "1",
-      question: "The condition in an if/else statement is enclosed with ________",
-      options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-      correct: "curly brackets",
+        id: "1",
+        question: "The condition in an if/else statement is enclosed with ________",
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correct: "curly brackets",
     },
     {
-      id: "2",
-      question: "Arrays in JavaScript can be used to store _________",
-      options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      correct: "all of the above",
+        id: "2",
+        question: "Arrays in JavaScript can be used to store _________",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correct: "all of the above",
     },
     {
-      id: "3",
-      question: "String values must be enclosed within _________ when being assigned to variables.",
-      options: ["commas", "curly brackets", "quotes", "parenthesis"],
-      correct: "quotes",
+        id: "3",
+        question: "String values must be enclosed within _________ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parenthesis"],
+        correct: "quotes",
     },
     {
-      id: "4",
-      question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
-      options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-      correct: "console.log",
+        id: "4",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correct: "console.log",
     },
   ];
+
+// Added an event listener to the restart button so that the quiz restarts when this button is clicked.
+restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide");
+    scoreContainer.classList.add("hide");
+  });
   
 // Added an event listener to the submit button so that the answer is submitted and the next questino shows up.
 submitBtn.addEventListener(
-    "click",
-    (displayNext = () => {
+    "click", (displayNext = () => {
       // This increases the question count that will be displayed on the HTML each time. 
       questionCount += 1;
-      // if last question
+      //if last question
       if (questionCount == quizArray.length) {
-        // hide question container and display score
+        //hide question container and display score
         displayContainer.classList.add("hide");
         scoreContainer.classList.remove("hide");
         // This displays the user score to the HTML file.
-        userScore.innerHTML =
+        userScore.innerHTML = 
           "Your score is " + scoreCount + " out of " + questionCount;
       } else {
-        // display questionCount
+        //display questionCount
         countOfQuestion.innerHTML =
           questionCount + 1 + " of " + quizArray.length + " Question";
         // This displays the quiz and runs the needed functions.
         quizDisplay(questionCount);
-        count = 75;
-        clearInterval(countdown);
-        timerDisplay();
       }
     })
   );
@@ -78,28 +82,46 @@ submitBtn.addEventListener(
 // Added a countdown timer by using a set interval function.
 const timerDisplay = () => {
     countdown = setInterval(() => {
-      count--;
-      timeLeft.innerHTML = `${count}s`;
-      if (count == 0) {
+      elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+      timeLeft.innerHTML = `${75 - elapsedTime}s`;
+      if (elapsedTime >= 75) {
         clearInterval(countdown);
-        displayNext();
       }
     }, 1000);
   };
 
-// This is the function we will call later on in the js file to run the quiz.
-function quizCreator() {
+
+const quizDisplay = (questionCount) => {
+    let quizCards = document.querySelectorAll(".container-mid");
+    
+    quizCards.forEach((card) => {
+      card.classList.add("hide");
+    });
+   
+    quizCards[questionCount].classList.remove("hide");
+  };
+  
+  // This is the function we will call later on in the js file to run the quiz.
+  function quizCreator() {
+    startTime = Date.now();
+    timerDisplay();
+
     // This randomnly sorts the question order so it is different each time.
     quizArray.sort(() => Math.random() - 0.5);
+   
     // This is the for statement that will generate the quiz.
     for (let i of quizArray) {
-      // This randomnly sorts the question answer options so it is different each time.
+      
+        // This randomnly sorts the question answer options so it is different each time.
       i.options.sort(() => Math.random() - 0.5);
+      
       // This creates a div in the html that will display the question and the answers.
       let div = document.createElement("div");
       div.classList.add("container-mid", "hide");
-      // This specifies the question number that will be displayed on the HTML
-      countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
+      
+      // This specifies the question number that will be displayed on the HTML.
+      countOfQuestion.innerHTML = "Question " + 1 + " of " + quizArray.length;
+      
       // This uses js to display the correct question.
       let question_DIV = document.createElement("p");
       question_DIV.classList.add("question");
@@ -117,8 +139,8 @@ function quizCreator() {
     }
   }
 
-// This calls a function to check if option is correct or not and then add the necessary points to their score. 
-function checker(userOption) 
+// This function determines the initial layout of the webpage upon visiting the site. 
+function checker(userOption) {
     let userSolution = userOption.innerText;
     let question =
       document.getElementsByClassName("container-mid")[questionCount];
@@ -129,7 +151,7 @@ function checker(userOption)
       scoreCount++;
     } else {
       userOption.classList.add("incorrect");
-      // For marking the correct option
+      //For marking the correct option
       options.forEach((element) => {
         if (element.innerText == quizArray[questionCount].correct) {
           element.classList.add("correct");
@@ -137,28 +159,34 @@ function checker(userOption)
       });
     }
 
-// This function determines the initial layout of the webpage upon visiting the site. 
-function initial() {
-    quizContainer.innerHTML = "";
-    questionCount = 0;
-    scoreCount = 0;
-    count = 11;
+    //clear interval(stop timer)
     clearInterval(countdown);
-    timerDisplay();
-    quizCreator();
-    quizDisplay(questionCount);
+    //disable all options
+    options.forEach((element) => {
+      element.disabled = true;
+    });
   }
 
-
+// This function determines the initial layout of the webpage upon visiting the site. 
+function initial() {
+  quizContainer.innerHTML = "";
+  questionCount = 0;
+  scoreCount = 0;
+  count = 75;
+  clearInterval(countdown);
+  timerDisplay();
+  quizCreator();
+  quizDisplay(questionCount);
+}
 // This adds an event listener for when the user hits the start button that will begin the quiz. 
 startButton.addEventListener("click", () => {
-    startScreen.classList.add("hide");
-    displayContainer.classList.remove("hide");
-    initial();
-  });
+  startScreen.classList.add("hide");
+  displayContainer.classList.remove("hide");
+  initial();
+});
 
 // This tells the website to hide the quiz upon initially loading, until the user hits the start button.
 window.onload = () => {
-    startScreen.classList.remove("hide");
-    displayContainer.classList.add("hide");
-  };
+  startScreen.classList.remove("hide");
+  displayContainer.classList.add("hide");
+};
