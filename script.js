53e61fb4440b7f1991459fcdfcf7a86bfd67f0ea
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-02-20 19:47:08 -0500

    Created an array with the quiz questions inside of it.

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ const timeLimit = 75; // set the time in seconds
 let currentQuestionIndex;
 let timeLeft;
 let timerId;
+
+// Created an array with the quiz questions inside of it. 
+const questions = [
+    {
+        question: "Commonly used data types do NOT include: ",
+        options: ["strings", "numbers", "alerts", "booleans"],
+        answerIndex: 2
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ___________",
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answerIndex: 1
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____________",
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answerIndex: 3
+    },
+    {
+        question: "String values must be encolsed within _________ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answerIndex: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answerIndex: 3
+    },
+];
